f300185799592277a3dbf8e8b9df6acffa1d6550
AlexanderSaid <alexander.t.said@gmail.com>
2022-06-19 05:11:21 +0200

    build editedDate fuction to get time since last edit

---
+++ src/utils/editedDate.ts
@@ -0,0 +1,27 @@
+export const editedDate = (date: string) => {
+  const currentDate = new Date();
+  const updatedDate = new Date(date);
+  const diff = currentDate.getTime() - updatedDate.getTime();
+  const minutes = Math.floor(diff / 1000 / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
+  console.log(diff, days, hours, minutes, months, years);
+  if (years > 0) {
+    return `over ${years} years ago`;
+  }
+  if (months > 0) {
+    return `${months} months ago`;
+  }
+  if (days > 0) {
+    return `${days} days ago`;
+  }
+  if (hours > 0) {
+    return `${hours} hours ago`;
+  }
+  if (minutes > 0) {
+    return `${minutes} minutes ago`;
+  }
+  return 'just now';
+};
